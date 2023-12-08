93eb0d385facd9b9ea57eb5f0ec02d03b71e9b12
yildirimege <ege.yildirim@xoxgames.io>
2023-12-08 13:52:13 +0300

    Initial commit

---
+++ SmileyCounter/SmileyCounter.cpp
@@ -0,0 +1,12 @@
+﻿// SmileyCounter.cpp : Defines the entry point for the application.
+//
+
+#include "SmileyCounter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
